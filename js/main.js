4f9a7eb1abefd7fbedc53f578719965c47622ccd
Anna Luiza <luizafistarol@gmail.com>
2023-01-21 02:59:17 -0300

    Transformando o input em valor aceito pela API

--- js/main.js
+++ js/main.js
@@ -6,4 +6,9 @@ btnBusca.addEventListener("click", transformaNomePersonagem);
 
 function transformaNomePersonagem() {
     let nome = nomePersonagemInput.value.toLowerCase();
+
+    nomeTransformado = nome
+        .split(" ")
+        .map(nome => nome.charAt(0).toUpperCase() + nome.slice(1))
+        .join(" ");
 }
