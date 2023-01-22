cebb784d86978f1f819225b1f6e316e30f193685
Anna Luiza <luizafistarol@gmail.com>
2023-01-22 15:36:12 -0300

    Adicionando evento enter input

--- js/main.js
+++ js/main.js
@@ -4,13 +4,15 @@ const nomePersonagemInput = document.getElementById("input-personagem");
 const btnBusca = document.getElementById("busca");
 let nomeTransformado = "";
 
+nomePersonagemInput.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+    transformaNomePersonagem();
+    }
+  });
 btnBusca.addEventListener("click", transformaNomePersonagem);
 
 async function transformaNomePersonagem() {
-    filmesPersonagem.innerHTML = ""
-    const xpto = nomeTransformado || "oi"
-    console.log("xpto", xpto)
-
+    filmesPersonagem.innerHTML = "";
     let nome = nomePersonagemInput.value.toLowerCase();
 
     nomeTransformado = nome
@@ -19,19 +21,20 @@ async function transformaNomePersonagem() {
         .join(" ");
     
     await buscaImagemNASA();
+    nomePersonagemInput.value = "";    
 }
 
-
+//IGNORAR ESPAÇOS APÓS NOME PERSONAGEM
 //@ Capturando os dados da API da Disney e integrando ao DOM
 async function buscaImagemNASA() {
     const consultaDadosPersonagem = await fetch(`https://api.disneyapi.dev/character?name=${nomeTransformado}`);
     const dadosConvertidos = await consultaDadosPersonagem.json();
     const arrayPersonagem = dadosConvertidos.data
+    console.log(arrayPersonagem)
 
     const personagem = arrayPersonagem.find((i) => {
         return (i.name == nomeTransformado)
     })
-    console.log(personagem)
     const arrayFilmes = personagem.films
     const imgPersonagem = personagem.imageUrl
 
