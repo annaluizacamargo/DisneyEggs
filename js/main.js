73d696af38fa1fd6a1e01089ff43bc0710ef633e
Anna Luiza <luizafistarol@gmail.com>
2023-01-22 14:56:17 -0300

    Corrigindo bug de busca de personagens

--- js/main.js
+++ js/main.js
@@ -7,9 +7,9 @@ let nomeTransformado = "";
 btnBusca.addEventListener("click", transformaNomePersonagem);
 
 async function transformaNomePersonagem() {
-    filmesPersonagem.innerHTML = "";
-    const xpto = nomeTransformado || "oi";
-    console.log("xpto", xpto);
+    filmesPersonagem.innerHTML = ""
+    const xpto = nomeTransformado || "oi"
+    console.log("xpto", xpto)
 
     let nome = nomePersonagemInput.value.toLowerCase();
 
@@ -18,16 +18,41 @@ async function transformaNomePersonagem() {
         .map(nome => nome.charAt(0).toUpperCase() + nome.slice(1))
         .join(" ");
     
-    await buscaPersonagemAPI();
+    await buscaImagemNASA();
 }
 
 
 //@ Capturando os dados da API da Disney e integrando ao DOM
-async function buscaPersonagemAPI() {
+async function buscaImagemNASA() {
     const consultaDadosPersonagem = await fetch(`https://api.disneyapi.dev/character?name=${nomeTransformado}`);
     const dadosConvertidos = await consultaDadosPersonagem.json();
-    //console.log(dadosConvertidos)
-    const arrayPersonagem = dadosConvertidos.data;
+    const arrayPersonagem = dadosConvertidos.data
+
+    const personagem = arrayPersonagem.find((i) => {
+        return (i.name == nomeTransformado)
+    })
+    console.log(personagem)
+    const arrayFilmes = personagem.films
+    const imgPersonagem = personagem.imageUrl
+    console.log(arrayFilmes)
+    console.log(imgPersonagem)
+
+    //arrayPersonagem.forEach((i) => {
+    //    const nomePersonagens = i.name
+    //    console.log(nomePersonagens)
+    //    if(i.name == nomeTransformado){
+    //        const arrayFilmes = i.films
+    //        const imgPersonagem = i.imageUrl
+    //        exibeFilmes(arrayFilmes)
+    //        exibeFotoPersonagem(imgPersonagem)            
+    //        console.log("IGUAL")
+    //    }
+    //})
+
+    console.log(arrayPersonagem)
+    
+    //imgAPI.setAttribute("src", `${dadosConvertidos.url}`);
+    //linkImgAPI.setAttribute("href", `${dadosConvertidos.url}`);
 }
 
 const nomePersonagem = document.getElementById("name-api")
